5fc4aaa4793d19aef4fb5fab8dc98c7272703d67
Serge Yager <sergeyager@gmail.com>
2014-04-08 19:41:22 -0700

    Added comments
    Went through and commented out things

--- project.py
+++ project.py
@@ -22,6 +22,7 @@ class round:
 		return players
 
 class tournament:
+	#sets number of players
 	n = 18
 	players = [0]*n
 	#this repeats the round and calculates the winner
@@ -29,7 +30,7 @@ class tournament:
 		x = round()
 		for i in range(4):
 			self.players = x.match(self.players)
-		# find winners
+		# finds if there is a clear winner after x rounds
 	def winner(self):
 		if self.players[0] == self.players[1]:
 			return False
@@ -37,12 +38,13 @@ class tournament:
 			return True
 
 class count:
+	#core to figuring out if a 5th round is needed
 	def counter(self, tournaments):
 		total = 0
 		t = tournament()
 		for i in range(tournaments):
 			t.play()
-			# count if winners > 2
+			# count if there is a clear winner, if no add 1
 			if t.winner() == False:
 				total += 1
 		percentage = float(total)/float(tournaments) * 100
@@ -53,3 +55,5 @@ class count:
 t = tournament()
 t.play()
 
+#if testing individual features, use t
+#if testing for % of draw, use c
