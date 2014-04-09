b52236972664c3cbd7c4fc2a5a8f25792882f3e0
Serge Yager <sergeyager@gmail.com>
2014-04-08 18:49:34 -0700

    Converted into object-oriented
    Re did the entire project as object-oriented
    Added round class
    Added tournament class
    Added counter class
    -possible bugs: results are too small
    -code seems sound

--- project.py
+++ project.py
@@ -1,26 +1,51 @@
-#n is number of players
-n=18
-players = [0]*n
 import random
 
+class round:
+	def match(self, players):
+		#organizes players in a top down structure based on score
+		for i in range(len(players)/2):
+			#assigns players Win/Loss/Tie
+			result = random.random()
+			if result < 0.475:
+				players[i*2+1] += 3
+			elif result > 0.95:
+				players[i*2+1] += 1
+				players[i*2] += 1
+			else:
+				players[i*2] += 3
+		players.sort.()
+		players.reverse()
+		return players
 
-def round(players):
-	#organizes players in a top down structure
-	players.sort()
-	players.reverse()
-	for i in range(len(players)/2):
-		#assigns players Win/Loss/Tie
-		result = random.random()
-		if result < 0.475:
-			players[i*2+1] += 3
-		elif result > 0.95:
-			players[i*2+1] += 1
-			players[i*2] += 1
+class tournament:
+	n = 18
+	players = [0]*n
+	#this repeats the round and calculates the winner
+	def play(self):
+		x = round()
+		for i in range(4):
+			self.players = x.match(self.players)
+		# find winners
+	def winner(self):
+		if self.players[0] == self.players[1]:
+			return False
 		else:
-			players[i*2] += 3
-	print players
+			return True
 
-round(players)
-round(players)
-round(players)
-round(players)
+class count:
+	def counter(self, tournaments):
+		total = 0
+		t = tournament()
+		for i in range(tournaments):
+			t.play()
+			# count if winners > 2
+			if t.winner() == False:
+				total += 1
+		percentage = float(total)/float(tournaments) * 100
+		print "The chance of a 5th round is " + str(percentage) + "%."
+
+c = count()
+c.counter(50000)
+#t = tournament()
+#t.play(players)
+
